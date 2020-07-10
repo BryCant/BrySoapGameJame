864a9ca634646a06084e9dab81d5fb70f34b24a6
Bryant <33697753+BryCant@users.noreply.github.com>
2020-07-10 18:15:36 -0500

    Place holders and Logic added
    Mock ship, instantiated boost sprites, spacey movement

---
+++ BrySoap_GameJam/Assets/Scripts/Ship_Movement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship_Movement : MonoBehaviour
+{
+    public Transform Booster1;
+    public Transform Booster2;
+    public Transform Booster3;
+    public Transform Booster4;
+
+    private Vector3 V3_Zero = Vector3.zero;
+
+    public Rigidbody2D ship_rb;
+    public GameObject boostPrefab;
+
+    float horizontalAxis = 0f;
+    float verticalAxis = 0f;
+    public float floatiness = 1f;
+    public float boostSpeed = 30f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Ship Control
+        horizontalAxis = Input.GetAxisRaw("Horizontal") * boostSpeed * 10f;
+        verticalAxis = Input.GetAxisRaw("Vertical") * boostSpeed * 10f;
+
+        if (horizontalAxis < 0)
+        {
+            Instantiate(boostPrefab, Booster2.position, Booster2.rotation);
+        }
+        else if (horizontalAxis > 0)
+        {
+            Instantiate(boostPrefab, Booster4.position, Booster4.rotation);
+        }
+
+        if (verticalAxis < 0)
+        {
+            Instantiate(boostPrefab, Booster1.position, Booster1.rotation);
+        }
+        else if (verticalAxis > 0)
+        {
+            Instantiate(boostPrefab, Booster3.position, Booster3.rotation);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 targetVelocity = new Vector2(horizontalAxis * Time.fixedDeltaTime * 10f, verticalAxis * Time.fixedDeltaTime * 10f);
+        ship_rb.velocity = Vector3.SmoothDamp(ship_rb.velocity, targetVelocity, ref V3_Zero, floatiness);
+
+    }
+
+}
