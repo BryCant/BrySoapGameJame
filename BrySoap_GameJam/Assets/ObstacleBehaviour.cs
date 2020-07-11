2e42f0b3174bac65ec2b24322db388ce5c055a9b
Bryant <33697753+BryCant@users.noreply.github.com>
2020-07-11 12:12:21 -0500

    Obstacle spawn is in
    obstacle randomly spawn and die after off screen. They all the same tho

---
+++ BrySoap_GameJam/Assets/ObstacleBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleBehaviour : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public float obstacleSpeed = 5f;
+
+    void Start()
+    {
+        rb.velocity = transform.right * -obstacleSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("ObstacleDeath"))
+        {
+            Destroy(gameObject);
+        }
+        /*else if (other.gameObject.CompareTag("platform"))
+        {
+            Destroy(gameObject);
+        }*/
+    }
+}
--- BrySoap_GameJam/Assets/Scripts/ObstacleSpawner.cs
+++ BrySoap_GameJam/Assets/Scripts/ObstacleSpawner.cs
@@ -5,26 +5,35 @@ using UnityEngine.UIElements;
 
 public class ObstacleSpawner : MonoBehaviour
 {
-
-    public GameObject[] obstacles;
+    // public Transform spawnPoint;
+    public GameObject obstacle1;
+    public GameObject obstacle2;
 
     public float spawnTimeInc = 10f;
     private float spawnTime = 0f;
-    
+    public int obLength = 2;
+
 
     void Update()
     {
         if (Time.time >= spawnTime)
         {
             spawnTime = Time.time + spawnTimeInc;
+            SpawnObstacles();
+        }
+    }
 
-            int numOfObstacles = Random.Range(1,5);
-
-            for (int obstacle = 0; obstacle < numOfObstacles; obstacle++)
-            {
-                int randomObstacleIndex = Random.Range(0, obstacles.Length);
-                Instantiate(obstacles[randomObstacleIndex], transform.position, transform.rotation);
-            }
+    void SpawnObstacles()
+    {
+        int chosenObstacle = Random.Range(0, obLength);
+        Vector3 randPos = transform.position + new Vector3(0, Random.Range(0, 10), 0);
+        if (chosenObstacle == 0)
+        {
+            Instantiate(obstacle1, randPos, transform.rotation);
+        }
+        else if (chosenObstacle == 1)
+        {
+            Instantiate(obstacle2,  randPos, transform.rotation);
         }
     }
 }
