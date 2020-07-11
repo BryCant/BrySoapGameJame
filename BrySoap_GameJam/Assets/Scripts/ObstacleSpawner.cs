78eb337259d148a33c2dd0f253e0ec7c109dc44f
Bryant <33697753+BryCant@users.noreply.github.com>
2020-07-11 09:51:19 -0500

    Random Spawners
    spawner implemented

---
+++ BrySoap_GameJam/Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+
+    public GameObject[] obstacles;
+
+    public float spawnTimeInc = 10f;
+    private float spawnTime = 0f;
+    
+
+    void Update()
+    {
+        if (Time.time >= spawnTime)
+        {
+            spawnTime = Time.time + spawnTimeInc;
+
+            int numOfObstacles = Random.Range(1,5);
+
+            for (int obstacle = 0; obstacle < numOfObstacles; obstacle++)
+            {
+                int randomObstacleIndex = Random.Range(0, obstacles.Length);
+                Instantiate(obstacles[randomObstacleIndex], transform.position, transform.rotation);
+            }
+        }
+    }
+}
