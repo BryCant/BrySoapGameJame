70ebe2e1ae6ccdb78b80c75abd375c9579c25b21
Bryant <33697753+BryCant@users.noreply.github.com>
2020-07-12 12:47:59 -0500

    pause menu
    but no stop

--- BrySoap_GameJam/Assets/PauseLogic.cs
+++ BrySoap_GameJam/Assets/PauseLogic.cs
@@ -5,11 +5,25 @@ using UnityEngine;
 
 public class PauseLogic : MonoBehaviour
 {
+    public GameObject pauseScreen;
     public GameObject tutScreen;
 
+    public void GoHome()
+    {
+        SceneManager.LoadScene(0);
+    }
     public void Restart()
     {
         SceneManager.LoadScene(1);
+    }
+    public void LoadPause()
+    {
+        pauseScreen.SetActive(true);
+    }
+
+    public void EndPause()
+    {
+        pauseScreen.SetActive(false);
     }
 
     public void LoadTut()
