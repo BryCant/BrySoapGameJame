8f22f59a62c14c4d002ad8d2334ef86b2a4a3288
Bryant <33697753+BryCant@users.noreply.github.com>
2020-07-12 12:54:31 -0500

    Time Stops
    Update to pause logic

--- BrySoap_GameJam/Assets/PauseLogic.cs
+++ BrySoap_GameJam/Assets/PauseLogic.cs
@@ -10,29 +10,35 @@ public class PauseLogic : MonoBehaviour
 
     public void GoHome()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene(0);
     }
     public void Restart()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene(1);
     }
     public void LoadPause()
     {
         pauseScreen.SetActive(true);
+        Time.timeScale = 0;
     }
 
     public void EndPause()
     {
+        Time.timeScale = 1;
         pauseScreen.SetActive(false);
     }
 
     public void LoadTut()
     {
+        Time.timeScale = 0;
         tutScreen.SetActive(true);
     }
 
     public void EndTut()
     {
+        Time.timeScale = 0;
         tutScreen.SetActive(false);
     }
 }
