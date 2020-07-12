fdbdc47df84a5d19d3f2b60f822a70e6adbc33d9
Bryant <33697753+BryCant@users.noreply.github.com>
2020-07-12 00:36:28 -0500

    Controls In
    really hard if you get hit

--- BrySoap_GameJam/Assets/Scripts/Ship_Movement.cs
+++ BrySoap_GameJam/Assets/Scripts/Ship_Movement.cs
@@ -8,6 +8,7 @@ public class Ship_Movement : MonoBehaviour
 
     public Rigidbody2D ship_rb;
 
+    public Animator shipAnim;
     public Animator b1Anim;
     public Animator b2Anim;
     public Animator b3Anim;
@@ -18,12 +19,40 @@ public class Ship_Movement : MonoBehaviour
     public float floatiness = 1f;
     public float boostSpeed = 30f;
 
+    private int glitchVersion = 0;
+
     // Update is called once per frame
     void Update()
     {
         // Ship Control
-        horizontalAxis = Input.GetAxisRaw("Horizontal") * boostSpeed * 10f;
-        verticalAxis = Input.GetAxisRaw("Vertical") * boostSpeed * 10f;
+        switch (glitchVersion)
+        {
+            case 0:
+                horizontalAxis = Input.GetAxisRaw("Horizontal") * boostSpeed * 10f;
+                verticalAxis = Input.GetAxisRaw("Vertical") * boostSpeed * 10f;
+                break;
+            case 1:
+                horizontalAxis = Input.GetAxisRaw("Vertical") * boostSpeed * 10f;
+                verticalAxis = Input.GetAxisRaw("Horizontal") * boostSpeed * 10f;
+                break;
+            case 2:
+                horizontalAxis = Input.GetAxisRaw("Horizontal") * boostSpeed * -10f;
+                verticalAxis = Input.GetAxisRaw("Vertical") * boostSpeed * -10f;
+                break;
+            case 3:
+                horizontalAxis = Input.GetAxisRaw("Vertical") * boostSpeed * -10f;
+                verticalAxis = Input.GetAxisRaw("Horizontal") * boostSpeed * -10f;
+                break;
+            case 4:
+                horizontalAxis = Input.GetAxisRaw("Vertical") * boostSpeed * -10f;
+                verticalAxis = Input.GetAxisRaw("Horizontal") * boostSpeed * 10f;
+                break;
+            case 5:
+                horizontalAxis = Input.GetAxisRaw("Vertical") * boostSpeed * 10f;
+                verticalAxis = Input.GetAxisRaw("Horizontal") * boostSpeed * -10f;
+                break;
+
+        }
 
         if (horizontalAxis < 0)
         {
@@ -53,11 +82,30 @@ public class Ship_Movement : MonoBehaviour
 
     }
 
+    void Glitch()
+    {
+        int newRandomGlitchVersion = Random.Range(1, 5);
+        glitchVersion = newRandomGlitchVersion;
+        shipAnim.SetBool("IsGlitching", true);
+
+    }
+
     void FixedUpdate()
     {
         Vector3 targetVelocity = new Vector2(horizontalAxis * Time.fixedDeltaTime * 10f, verticalAxis * Time.fixedDeltaTime * 10f);
         ship_rb.velocity = Vector3.SmoothDamp(ship_rb.velocity, targetVelocity, ref V3_Zero, floatiness);
         ResetBoosters();
+        shipAnim.SetBool("IsGlitching", false);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Ship Hit");
+            Glitch();
+        }
     }
 
 }
